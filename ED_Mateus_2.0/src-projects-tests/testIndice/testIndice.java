18a36afd45f80aeec1d6a472b2be88b640f39090
Mateus Luiz <mateuslfb@hotmail.com>
2016-06-23 13:47:24 -0300

    IndiceRemissivo finished

--- ED_Mateus_2.0/src-projects-tests/testIndice/testIndice.java
+++ ED_Mateus_2.0/src-projects-tests/testIndice/testIndice.java
@@ -1,7 +1,46 @@
 package testIndice;
 
+import IndiceRemissivo.IndiceRemissivo;
 import junit.framework.TestCase;
 
 public class testIndice extends TestCase {
+	
+	IndiceRemissivo indice = new IndiceRemissivo();
+	
+	public void setUp(){
+		
+		indice.populateMap("and");
+		indice.populateMap("be");
+		indice.populateMap("by");
+		indice.populateMap("easy");
+		indice.populateMap("human-engineered");
+		indice.populateMap("programming");
+		indice.populateMap("programs");
+		indice.populateMap("to");
+		
+		
+	}
+	
+	public void testAddMap(){
+		
+		assertEquals(true, indice.findKeyword("and"));
+		assertEquals(true, indice.findKeyword("be"));
+		assertEquals(true, indice.findKeyword("by"));
+		assertEquals(true, indice.findKeyword("easy"));
+		assertEquals(true, indice.findKeyword("human-engineered"));
+		assertEquals(true, indice.findKeyword("programming"));
+		assertEquals(true, indice.findKeyword("programs"));
+		assertEquals(true, indice.findKeyword("to"));
+		assertEquals(false, indice.findKeyword("robot"));
+		assertEquals(false, indice.findKeyword("Java"));
+		assertEquals(false, indice.findKeyword("hue"));
+	}
+	
+	public void testCheck(){
+		
+		indice.check();
+		indice.resultArchive(); //arquivo é gerado na pasta do projeto, apos executar apenas rode um "Refresh"
+		
+	}
 
 }
---
+++ ED_Mateus_2.0/src-projects/IndiceRemissivo/IndiceRemissivo.java
@@ -0,0 +1,117 @@
+package IndiceRemissivo;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+import Lists.LinkedListGeneric;
+
+
+public class IndiceRemissivo {
+	
+	HashMap<String, LinkedListGeneric<String>> map;
+
+	
+	public IndiceRemissivo(){
+		map = new HashMap<String, LinkedListGeneric<String>>();
+	}
+	
+	public void populateMap(String keyWord){
+		map.put(keyWord, new LinkedListGeneric<String>());
+	}
+	
+	public boolean findKeyword(String keyWord){
+		return map.containsKey(keyWord);
+	}
+
+	public void check() {
+		String file = "text.txt";
+		String line = null;
+		 
+		try { // vai ler as linhas do arquivo
+	            FileReader fileReader = new FileReader(file);
+
+	            BufferedReader bufferedReader =  new BufferedReader(fileReader);
+	            
+	            while((line = bufferedReader.readLine()) != null) {
+	            	line = line.replaceAll(",", "");
+	            	checkLine(line);
+	            } 
+	            
+	            
+	            bufferedReader.close();         
+	    }catch(FileNotFoundException ex) {
+	            System.out.println( "Unable to open file '" + file + "'");                
+        }catch(IOException ex) {
+	            System.out.println("Error reading file '"  + file + "'");                  
+        }
+		
+	}
+	
+	protected void checkLine(String line){
+		StringTokenizer lineTokens = new StringTokenizer(line); // linha para procurar a palavra
+		StringTokenizer lineTokens2 = new StringTokenizer(line); // linha para procurar o numero
+		String token2 = "";
+		String token = "";
+		String number= "";
+		
+		while(true){
+			
+			token2 = lineTokens2.nextToken();
+			
+			if(isNumeric(token2)){
+				number = token2;
+				break;
+			}
+		}
+		
+		while(lineTokens.hasMoreTokens()){
+			token = lineTokens.nextToken().toLowerCase();
+			for(String s : map.keySet()){
+				if (s.equals(token)){
+					map.get(s).insert(0, number);
+				}
+			}
+		}
+	}
+	
+	protected static boolean isNumeric(String str){
+	    for (char c : str.toCharArray())
+	    {
+	        if (!Character.isDigit(c)) return false;
+	    }
+	    return true;
+	}
+	
+	public HashMap<String, LinkedListGeneric<String>> getMap(){
+		return map;
+	}
+	
+	public void resultArchive(){
+		
+		try {
+			PrintWriter archive = new PrintWriter("result.txt", "UTF-8");
+			
+			for(String s: map.keySet()){
+				archive.print(s + ": ");
+				for(int i = 0; i < map.get(s).size(); i++){
+					archive.print(map.get(s).elementAt(i) + " ");
+				}
+				archive.println();
+			}
+			
+			archive.close();
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
